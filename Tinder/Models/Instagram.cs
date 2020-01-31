6d29fb198a14531577f602d197f662a19dd0ba20
Taner Özel <tanerozel47@gmail.com>
2020-02-01 02:21:02 +0300

    added instagram model

--- Tinder/Models/Instagram.cs
+++ Tinder/Models/Instagram.cs
@@ -1,6 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
 namespace Tinder.Models
 {
     public class Instagram
     {
+        [JsonPropertyName("last_fetch_time")]
+        public DateTime LastFetchTime { get; set; }
+        [JsonPropertyName("completed_initial_fetch")]
+        public bool CompletedInitialFetch { get; set; }
+
+        [JsonPropertyName("media_count")]
+        public int MediaCount { get; set; }
+
+        public IReadOnlyList<Photos> CommonPhotos { get; set; } = default!;
+        public class Photos
+        {
+            [JsonPropertyName("image")]
+            public string Image { get; set; } = default!;
+
+            [JsonPropertyName("thumbnail")]
+            public string Thumbnail { get; set; } = default!;
+
+            [JsonPropertyName("ts")]
+            public string Ts { get; set; } = default!;
+        }
+
     }
 }
