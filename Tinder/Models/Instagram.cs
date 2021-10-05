f9650a84ec5f875732a286896b73ffec776e10e6
Princess Swan <91006817+swanprincess385@users.noreply.github.com>
2021-10-05 02:57:13 -0800

    Revert "added instagram model (#9)"
    This reverts commit 3a70bfde04d89047aa6b55d8d8805123c223e332.

--- Tinder/Models/Instagram.cs
+++ Tinder/Models/Instagram.cs
@@ -1,37 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Text.Json.Serialization;
-
 namespace Tinder.Models
 {
     public class Instagram
     {
-        [JsonPropertyName("last_fetch_time")]
-        public DateTime LastFetchTime { get; set; }
-        [JsonPropertyName("completed_initial_fetch")]
-        public bool CompletedInitialFetch { get; set; }
-
-        [JsonPropertyName("media_count")]
-        public int MediaCount { get; set; }
-
-        [JsonPropertyName("profile_picture")]
-        public string ProfilePicture { get; set; } = default!;
-        [JsonPropertyName("username")]
-        public string UserName { get; set; } = default!;
-        [JsonPropertyName("should_reauthenticate")]
-        public Boolean ShouldReauthenticate { get; set; }
-        public IReadOnlyList<Photos> CommonPhotos { get; set; } = default!;
-        public class Photos
-        {
-            [JsonPropertyName("image")]
-            public string Image { get; set; } = default!;
-
-            [JsonPropertyName("thumbnail")]
-            public string Thumbnail { get; set; } = default!;
-
-            [JsonPropertyName("ts")]
-            public string Ts { get; set; } = default!;
-        }
-
     }
 }
